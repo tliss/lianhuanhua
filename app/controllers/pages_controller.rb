04bdca65212a4920821b74867a4c921cd5284db9
Taylor Liss <tayloraliss@gmail.com>
2019-05-06 18:11:21 -0400

    Add destroy method to pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -12,6 +12,10 @@ class PagesController < ApplicationController
     @page = @book.pages.new
   end
 
+  def edit
+
+  end
+
   def create
     @book = Book.find(params[:book_id])
     @page = @book.pages.create(page_params)
@@ -21,6 +25,16 @@ class PagesController < ApplicationController
     else
       render 'new'
     end
+  end
+
+  def update
+
+  end
+
+  def destroy
+    @page = Page.find(params[:id])
+    @page.destroy
+    redirect_to book_path(@page.book)
   end
 
   private
