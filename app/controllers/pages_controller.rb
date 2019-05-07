eb24adb5ae6cef1076f45671a4fbf4f6d1db8609
Taylor Liss <tayloraliss@gmail.com>
2019-05-07 17:54:03 -0400

    Add ability to edit pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -29,7 +29,13 @@ class PagesController < ApplicationController
   end
 
   def update
+    @page = Page.find(params[:id])
 
+    if @page.update(page_params)
+      redirect_to @page.book
+    else
+      render 'edit'
+    end
   end
 
   def destroy
