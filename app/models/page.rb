f8328e0ae5803091699749427c04bb2c133c1ac4
Taylor Liss <tayloraliss@gmail.com>
2019-05-31 00:29:35 -0400

    Links to next and previous page now correct

--- app/models/page.rb
+++ app/models/page.rb
@@ -5,4 +5,13 @@ class Page < ApplicationRecord
   def thumbnail
     self.picture.variant(resize: '100x100')
   end
+
+  def previous_page
+    self.class.where("page_number < ?", page_number).order(page_number: 'desc').first
+  end
+
+  def next_page
+    self.class.where("page_number > ?", page_number).order(page_number: 'asc').first
+  end
+
 end
