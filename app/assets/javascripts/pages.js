80bc43d2c53a38c372afca1041bd2927cf0bd61c
Taylor Liss <tayloraliss@gmail.com>
2019-06-18 19:26:15 -0400

    Fix phantom opacity problem

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -1,25 +1,27 @@
-$(document).on('turbolinks:load', function() {
-    if ($('#img-prev').length) {
-        $('#img-prev').hover(function() {
-            $(this).fadeTo(300, 0.3);
-        }, function() {
-            $(this).fadeTo(300, 0);
+    $(document).on('turbolinks:load', function() {
+        if ($('#img-prev').length) {
+            $('#img-prev').hover(function() {
+                $(this).fadeTo(300, 0.3);
+            }, function() {
+                $(this).fadeTo(300, 0);
+            });
+        }
+
+        if ($('#img-next').length) {
+            $('#img-next').hover(function() {
+                $(this).fadeTo(300, 0.3);
+            }, function() {
+                $(this).fadeTo(300, 0);
+            });
+        }
+
+        $('#img-next-link').click(function(){
+            $('#img-next-link').css('opacity', 0);
+            $('#img-prev-link').css('opacity', 0);
         });
 
-        $('#img-prev-link').click(function() {
-            $('#img-prev').css('opacity', 0);
-        })
-    }
-
-    if ($('#img-next').length) {
-        $('#img-next').hover(function() {
-            $(this).fadeTo(300, 0.3);
-        }, function() {
-            $(this).fadeTo(300, 0);
+        $('#img-prev-link').click(function(){
+            $('#img-next-link').css('opacity', 0);
+            $('#img-prev-link').css('opacity', 0);
         });
-
-        $('#img-next-link').click(function() {
-            $('#img-next').css('opacity', 0);
-        })
-    }
-});
+    });
